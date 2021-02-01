0d1df24dfdb21f88ddf980d3eea712d596f52c24
murphypetercl <murphy.petercl@gmail.com>
2021-02-01 14:55:28 -0500

    Ignore vault encrypted files

--- src/ansiblemdgen/AutoDocumenterBase.py
+++ src/ansiblemdgen/AutoDocumenterBase.py
@@ -2,7 +2,7 @@ from ansiblemdgen.Config import SingleConfig
 from ansiblemdgen.Utils import SingleLog
 import os
 from os import walk
-
+import yaml
 
 class WriterBase:
 
@@ -22,9 +22,9 @@ class WriterBase:
     def iterateOnFilesAndDirectories(self, directory, output_directory):
         for (dirpath, dirnames, filenames) in walk(directory):
             for filename in filenames:
-                #ignore any existing md files
-                if not filename.endswith('.md'):
-                    self.createMDFile(dirpath, filename, output_directory)
+                #ignore any existing md files and vault encrypted files
+                if not filename.endswith('.md') and self.isFileVaultEncrypted(dirpath, filename) is False:
+                        self.createMDFile(dirpath, filename, output_directory)
 
             for dirname in dirnames:
                 self.iterateOnFilesAndDirectories(dirpath+"/"+dirname, output_directory+"/"+dirname)
@@ -32,3 +32,11 @@ class WriterBase:
     def iterateOnCombinations(self, directory, combinations, output_directory):
         for combination in combinations:
             self.createMDCombinationFile(combination['filename'], directory, output_directory, combination['files_to_combine'])
+
+    def isFileVaultEncrypted(self, directory, filename):
+        with open(directory+"/"+filename, 'r') as stream:
+            data = stream.readlines()
+            if data[0].startswith('$ANSIBLE_VAULT;1.1;AES256'):
+                return True
+            else:
+                return False
